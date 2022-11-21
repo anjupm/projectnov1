c17229eb705ddac025420c61965a077cae492393
Anju <anjupm75@gmail.com>
2022-11-21 01:44:39 -0800

    commit and push

---
+++ bin/variables.dart
@@ -0,0 +1,14 @@
+void main(){
+ String name= "Anu";
+ int age=20;
+ int phone=6958741236;
+ double mark=8.6;
+ String email="anu10@gmal.com";
+
+ print("....My Resume....");
+ print("My name is $name");
+ print("My age is $age");
+ print("My phone number is $phone");
+ print("I have $mark cgpa in my graduation");
+ print("My email ID is $email");
+}
