60f0059958d49b859e209c3c486fa4dfe59ff3d9
Anju <anjupm75@gmail.com>
2022-11-20 23:26:52 -0800

    commit and push

---
+++ bin/projectnov1.dart
@@ -0,0 +1,5 @@
+import 'package:projectnov1/projectnov1.dart' as projectnov1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projectnov1.calculate()}!');
+}
---
+++ test/projectnov1_test.dart
@@ -0,0 +1,8 @@
+import 'package:projectnov1/projectnov1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
